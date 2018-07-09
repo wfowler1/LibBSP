d9bfd2e3d284c41b4d0dee782567ce71dcee7c94
wfowler <will.fowler4201@gmail.com>
2018-07-09 02:04:00 -0600

    Whoops, missed this one.

--- LibBSP/Source/Structs/Common/UIVertex.cs
+++ LibBSP/Source/Structs/Common/UIVertex.cs
@@ -1,7 +1,7 @@
 #if (UNITY_2_6 || UNITY_2_6_1 || UNITY_3_0 || UNITY_3_0_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5 || UNITY_4_0 || UNITY_4_0_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_5 || UNITY_4_6 || UNITY_5 || UNITY_5_3_OR_NEWER)
 #define UNITY
 #endif
-#if !(UNITY_4_6 || UNITY_5)
+#if !(UNITY_4_6 || UNITY_5 || UNITY_5_3_OR_NEWER)
 #if UNITY
 using UnityEngine;
 #else
