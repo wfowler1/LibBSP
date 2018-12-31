a07c8e86cfe98176583e656d0dc7f8ab97599c91
wfowler <will.fowler4201@gmail.com>
2018-12-31 15:04:48 -0700

    Properly revert Revision.cs when building, and add build configuration information to Revision.cs.

--- LibBSP/Source/Util/Revision.cs
+++ LibBSP/Source/Util/Revision.cs
@@ -2,5 +2,6 @@ namespace LibBSP {
 	public class Revision {
 		public const string version = "To be replaced on build";
 		public const string head = "To be replaced on build";
+		public const string configuration = "To be replaced on build";
 	}
 }
