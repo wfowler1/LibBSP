1eeaafe083b0579ec4fa8f14f9791d531b2d0448
wfowler <will.fowler4201@gmail.com>
2021-04-18 23:48:35 -0600

    Add constructor to LODTerrain to copy another LODTerrain using any given format.

--- LibBSP/Source/Structs/BSP/LODTerrain.cs
+++ LibBSP/Source/Structs/BSP/LODTerrain.cs
@@ -366,6 +366,50 @@ namespace LibBSP {
 		}
 
 		/// <summary>
+		/// Creates a new <see cref="LODTerrain"/> by copying the fields in <paramref name="source"/>, using
+		/// <paramref name="parent"/> to get <see cref="LibBSP.MapType"/> and <see cref="LumpInfo.version"/>
+		/// to use when creating the new <see cref="LODTerrain"/>.
+		/// If the <paramref name="parent"/>'s <see cref="BSP"/>'s <see cref="LibBSP.MapType"/> is different from
+		/// the one from <paramref name="source"/>, it does not matter, because fields are copied by name.
+		/// </summary>
+		/// <param name="source">The <see cref="LODTerrain"/> to copy.</param>
+		/// <param name="parent">
+		/// The <see cref="ILump"/> to use as the <see cref="Parent"/> of the new <see cref="LODTerrain"/>.
+		/// Use <c>null</c> to use the <paramref name="source"/>'s <see cref="Parent"/> instead.
+		/// </param>
+		public LODTerrain(LODTerrain source, ILump parent) {
+			Parent = parent;
+
+			if (parent != null && parent.Bsp != null) {
+				if (source.Parent != null && source.Parent.Bsp != null && source.Parent.Bsp.version == parent.Bsp.version && source.LumpVersion == parent.LumpInfo.version) {
+					Data = new byte[source.Data.Length];
+					Array.Copy(source.Data, Data, source.Data.Length);
+					return;
+				} else {
+					Data = new byte[GetStructLength(parent.Bsp.version, parent.LumpInfo.version)];
+				}
+			} else {
+				if (source.Parent != null && source.Parent.Bsp != null) {
+					Data = new byte[GetStructLength(source.Parent.Bsp.version, source.Parent.LumpInfo.version)];
+				} else {
+					Data = new byte[GetStructLength(MapType.Undefined, 0)];
+				}
+			}
+			
+			Flags = source.Flags;
+			Scale = source.Scale;
+			LightmapCoordinates = source.LightmapCoordinates;
+			UVs = source.UVs;
+			X = source.X;
+			Y = source.Y;
+			BaseZ = source.BaseZ;
+			TextureIndex = source.TextureIndex;
+			Lightmap = source.Lightmap;
+			VertexFlags = source.VertexFlags;
+			Heightmap = source.Heightmap;
+		}
+
+		/// <summary>
 		/// Factory method to parse a <c>byte</c> array into a <see cref="Lump{LODTerrain}"/>.
 		/// </summary>
 		/// <param name="data">The data to parse.</param>
